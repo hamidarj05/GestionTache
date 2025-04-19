36703f9b00f9bd177c9a427c6dbb52dcb5ad4bf5
hamidarj05 <hamidarj63@gmail.com>
2025-04-19 10:21:49 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,83 @@
+const listeTache = document.getElementById("taches")
+listeTache.style.listStyle = "none" 
+let taches = []
+for (let i = 1 ; i <= window.localStorage.length ;i++ ){
+    const tache = window.localStorage.getItem(i)
+    if (tache == "") {
+        break
+    }
+    const li = document.createElement("li")  
+    const div = document.createElement("div")  
+    div.style.float = "right"
+    var span = document.createElement("span")
+    span.style.color = "green"
+    span.style.cursor = "pointer"
+    span.textContent = "✔"
+    span.className = "me-2" 
+    span.addEventListener("click",()=>{
+        tacheDone(li)
+    })
+    div.appendChild(span)
+    var span = document.createElement("span")
+    span.style.color = "red"
+    span.style.cursor = "pointer"
+    span.textContent = "✖"  
+    span.addEventListener("click",()=>{
+        deleteTache(li)
+        window.localStorage.removeItem(i)
+    })
+    div.appendChild(span)
+    li.className = "tache"
+    li.textContent = tache
+    li.appendChild(div)
+    listeTache.appendChild(li)
+
+}
+
+
+function AjouterTache(){ 
+    const tache = document.getElementById("tache").value
+    window.localStorage.setItem(window.localStorage.length + 1,tache)
+    if(tache === ""){
+        alert("Kttb Chi7aja '-' ")
+    }else{ 
+        const li = document.createElement("li")  
+        const div = document.createElement("div")  
+        div.style.float = "right"
+        var span = document.createElement("span")
+        span.style.color = "green"
+        span.style.cursor = "pointer"
+        span.textContent = "✔"
+        span.className = "me-2" 
+        span.addEventListener("click",()=>{
+            tacheDone(li)
+        })
+        div.appendChild(span)
+        var span = document.createElement("span")
+        span.style.color = "red"
+        span.style.cursor = "pointer"
+        span.textContent = "✖"  
+        span.addEventListener("click",()=>{
+            deleteTache(li)
+        })
+        div.appendChild(span)
+        li.className = "tache"
+        li.textContent = tache
+        li.appendChild(div)
+        listeTache.appendChild(li)
+    }
+    document.getElementById("tache").value = ""
+}
+
+function tacheDone(li){
+    li.style.textDecoration = "line-through"
+    li.style.color = "green "
+}
+function deleteTache(li){
+    listeTache.removeChild(li)
+}
+
+// By Hamid
+
+
+// window.localStorage.clear()
